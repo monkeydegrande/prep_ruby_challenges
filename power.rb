f496a082cebf46debc0d0a6e41dc09742d8c1711
monkeydegrande <mikedegrande@gmail.com>
2015-05-21 12:17:57 -0400

    Added input functionality

--- power.rb
+++ power.rb
@@ -1,14 +1,18 @@
+puts 'Please enter your base number(only integers are accepted):'
+x = gets.to_i
+puts 'Please enter your exponent(only integers are accepted):'
+y = gets.to_i
 def power(x,y)
 	 	  
 if y == 0 
-	print 1
+	return 1
 elsif y < 0
 		num = x
 		(y * -1 - 1).times  {num *= x}
-		print ("1/" + num.to_s)
+		return ("1/" + num.to_s)
 else num = x
 		(y - 1).times  {num *= x}
-		print num
+		return num
 	end
 end
-power(3,-1)
+print power(x,y).to_s + " is your answer."
